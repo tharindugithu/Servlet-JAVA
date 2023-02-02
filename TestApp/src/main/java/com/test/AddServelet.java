de805494ef77eae05464ec65272aad151ea08bfa
tharindugithu <91211707+tharindugithu@users.noreply.github.com>
2023-02-02 16:21:20 +0530

    first-commit(basic servlet project)

---
+++ TestApp/src/main/java/com/test/AddServelet.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServelet extends HttpServlet {
+  
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {//you have to go with this method name
+	
+         int i = Integer.parseInt(req.getParameter("num1"));
+         int j = Integer.parseInt(req.getParameter("num2"));
+	     int k = i+j;
+	     PrintWriter out = res.getWriter();
+	     out.println("<h1>result is : </h1>"+k);
+	}
+}
