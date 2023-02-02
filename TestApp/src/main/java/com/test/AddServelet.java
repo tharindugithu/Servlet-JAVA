122da3e75dd0078f24229f242ec30d0af3ef5512
tharindugithu <atharindu325@gmail.com>
2023-02-03 00:55:28 +0530

    adding post and get methods

--- TestApp/src/main/java/com/test/AddServelet.java
+++ TestApp/src/main/java/com/test/AddServelet.java
@@ -9,10 +9,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServelet extends HttpServlet {
   
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {//you have to go with this method name
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {//only accept post reqvest
 	
          int i = Integer.parseInt(req.getParameter("num1"));//get the values in url
          int j = Integer.parseInt(req.getParameter("num2"));
+	     int k = i+j;//total value 
+	     PrintWriter out = res.getWriter();
+	     out.println("<h1>result is : </h1>"+k);//send to response client
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {//only accept get reqvest
+		
+        int i = Integer.parseInt(req.getParameter("num1"));//get the values in url
+        int j = Integer.parseInt(req.getParameter("num2"));
 	     int k = i+j;//total value 
 	     PrintWriter out = res.getWriter();
 	     out.println("<h1>result is : </h1>"+k);//send to response client
