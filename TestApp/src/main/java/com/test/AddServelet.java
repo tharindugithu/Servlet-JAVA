f1e3638050ddf50c09dd360e7bef937b317d5cdc
tharindugithu <atharindu325@gmail.com>
2023-02-02 18:49:32 +0530

    updated

--- TestApp/src/main/java/com/test/AddServelet.java
+++ TestApp/src/main/java/com/test/AddServelet.java
@@ -11,7 +11,7 @@ public class AddServelet extends HttpServlet {
   
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {//you have to go with this method name
 	
-         int i = Integer.parseInt(req.getParameter("num1"));
+         int i = Integer.parseInt(req.getParameter("num1"));//get the values in url
          int j = Integer.parseInt(req.getParameter("num2"));
 	     int k = i+j;//total value 
 	     PrintWriter out = res.getWriter();
