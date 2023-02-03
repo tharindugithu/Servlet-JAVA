8017ed2ff0b6d56f570904782a6776cdac59be6b
tharindugithu <atharindu325@gmail.com>
2023-02-03 12:47:55 +0530

    ServletContext & ServletConfig

---
+++ TestApp/src/main/java/com/test/MyServlet.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {//only accept get reqvest
+		PrintWriter out = res.getWriter();
+		out.print("<h1>Hi</h1>");
+		
+		//servlet context
+		ServletContext ctx  = getServletContext();
+		String str = ctx.getInitParameter("name");//go to web.xml and get the match value
+		
+		String str2 = ctx.getInitParameter("phone");//go to web.xml and get the match value
+		
+		//out.println(str);
+		//out.print("phone is "+str2);
+		
+		//servlet config
+		ServletConfig cg = getServletConfig();//get the values from specifically this servlet
+		String str3 = cg.getInitParameter("name");
+		
+		out.print(str3);
+	}
+}
